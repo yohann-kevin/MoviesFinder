27d8c7d18c170be90723bd9caf3b67f3c1fe925c
Perriguey <yo44prg@icloud.com>
2020-11-15 19:21:52 +0100

    feat : extract data

--- src/api.js
+++ src/api.js
@@ -11,6 +11,24 @@ export function api(id) {
         return response.json();
     }).then(function (json) {
         console.log(json);
+        console.log(
+            json.title,
+            json.tagline,
+            json.release_date,
+            json.genres,
+            json.runtime,
+            json.poster_path,
+            json.vote_average,
+            json.popularity,
+            json.vote_count,
+            json.homepage,
+            json.overview,
+            json.status,
+            json.budget,
+            json.revenue,
+            json.production_countries[0].name,
+            json.production_companies
+        )
         filmData = [json.title, json.overview, json.release_date, json.poster_path];
         viewsContent(filmData);
     })
