d8863cf78a76366db492a50858c6c0b96a28e949
Kirua <yo44prg@icloud.com>
2021-05-06 10:03:04 +0200

    feat: add fonts

--- src/App.vue
+++ src/App.vue
@@ -22,16 +22,35 @@ export default {
 </script>
 
 <style>
+@font-face {
+  font-family: "robotothin";
+  src: url("./assets/fonts/roboto/roboto-thin-webfont.woff") format("woff");
+  src: url("./assets/fonts/roboto/roboto-thin-webfont.woff2") format("woff2");
+}
+
+@font-face {
+  font-family: "robotoregular";
+  src: url("./assets/fonts/roboto/roboto-regular-webfont.woff") format("woff");
+  src: url("./assets/fonts/roboto/roboto-regular-webfont.woff2") format("woff2");
+}
+
+@font-face {
+  font-family: "robotobold";
+  src: url("./assets/fonts/roboto/roboto-bold-webfont.woff") format("woff");
+  src: url("./assets/fonts/roboto/roboto-bold-webfont.woff2") format("woff2");
+}
+
 * {
   margin: 0;
   padding: 0;
 }
+
 #app {
   width: 100%;
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
   background-color: #36393f;
-  /* font-family: 'robotothin'; */
+  font-family: 'robotothin';
 }
 </style>
