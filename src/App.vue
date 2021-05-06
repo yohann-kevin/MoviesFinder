189b4487a1f1e2a55e231412639e7a2ba42baf29
Kirua <yo44prg@icloud.com>
2021-05-06 09:31:09 +0200

    feat: add header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  background-color: #36393f;
+  /* font-family: 'robotothin'; */
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header id="header">
+    <h1 id="title">Movies Finder</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+#header {
+  width: 100%;
+  text-align: center;
+  background-color: #2f3136;
+}
+
+#title {
+  text-align: center;
+  padding: 10px;
+  color: #ffffff;
+}
+</style>
